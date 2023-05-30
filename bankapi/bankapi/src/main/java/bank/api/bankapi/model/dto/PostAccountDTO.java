4a72cf8eb0ca349112758e43dd16c737499ff341
BrandonInholland <42965932+BrandonInholland@users.noreply.github.com>
2023-05-30 23:16:17 +0200

    DTO's

---
+++ bankapi/bankapi/src/main/java/bank/api/bankapi/model/dto/PostAccountDTO.java
@@ -0,0 +1,108 @@
+package bank.api.bankapi.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import bank.api.bankapi.model.Account;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.Objects;
+
+/**
+ * PostAccountDTO
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-05-17T16:11:18.306Z[GMT]")
+
+
+public class PostAccountDTO {
+    @JsonProperty("userid")
+    private Long userid = null;
+
+    /**
+     * Gets or Sets accounttype
+     */
+
+    @JsonProperty("accounttype")
+    private Account.AccounttypeEnum accounttype = null;
+
+    public PostAccountDTO userid(Long userid) {
+        this.userid = userid;
+        return this;
+    }
+
+    /**
+     * Get userid
+     *
+     * @return userid
+     **/
+    @Schema(example = "212", description = "")
+
+    public Long getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Long userid) {
+        this.userid = userid;
+    }
+
+    public PostAccountDTO accounttype(Account.AccounttypeEnum accounttype) {
+        this.accounttype = accounttype;
+        return this;
+    }
+
+    /**
+     * Get accounttype
+     *
+     * @return accounttype
+     **/
+    @Schema(example = "current", description = "")
+
+    public Account.AccounttypeEnum getAccounttype() {
+        return accounttype;
+    }
+
+    public void setAccounttype(Account.AccounttypeEnum accounttype) {
+        this.accounttype = accounttype;
+    }
+
+
+    @Override
+    public boolean equals(java.lang.Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PostAccountDTO postAccountDTO = (PostAccountDTO) o;
+        return Objects.equals(this.userid, postAccountDTO.userid) &&
+                Objects.equals(this.accounttype, postAccountDTO.accounttype);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userid, accounttype);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class PostAccountDTO {\n");
+
+        sb.append("    userid: ").append(toIndentedString(userid)).append("\n");
+        sb.append("    accounttype: ").append(toIndentedString(accounttype)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(java.lang.Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
+}
---
+++ bankapi/bankapi/src/main/java/bank/api/bankapi/model/dto/PutAccountDTO.java
@@ -0,0 +1,139 @@
+package bank.api.bankapi.model.dto;
+
+import bank.api.bankapi.model.Account;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import bank.api.bankapi.model.Account;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+/**
+ * PutAccountDTO
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-05-17T16:11:18.306Z[GMT]")
+
+
+public class PutAccountDTO {
+    @JsonProperty("status")
+    private Account.StatusEnum status = null;
+    @JsonProperty("absoluteLimit")
+    private BigDecimal absoluteLimit = null;
+
+    public PutAccountDTO status(Account.StatusEnum status) {
+        this.status = status;
+        return this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return status
+     **/
+    @Schema(example = "open", description = "")
+
+    public Account.StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(Account.StatusEnum status) {
+        this.status = status;
+    }
+
+    public PutAccountDTO absoluteLimit(BigDecimal absoluteLimit) {
+        this.absoluteLimit = absoluteLimit;
+        return this;
+    }
+
+    /**
+     * Get absoluteLimit
+     *
+     * @return absoluteLimit
+     **/
+    @Schema(example = "0", description = "")
+
+    @Valid
+    public BigDecimal getAbsoluteLimit() {
+        return absoluteLimit;
+    }
+
+    public void setAbsoluteLimit(BigDecimal absoluteLimit) {
+        this.absoluteLimit = absoluteLimit;
+    }
+
+    @Override
+    public boolean equals(java.lang.Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PutAccountDTO putAccountDTO = (PutAccountDTO) o;
+        return Objects.equals(this.status, putAccountDTO.status) &&
+                Objects.equals(this.absoluteLimit, putAccountDTO.absoluteLimit);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(status, absoluteLimit);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class PutAccountDTO {\n");
+
+        sb.append("    status: ").append(toIndentedString(status)).append("\n");
+        sb.append("    absoluteLimit: ").append(toIndentedString(absoluteLimit)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(java.lang.Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
+
+    /**
+     * Gets or Sets status
+     */
+    public enum StatusEnum {
+        OPEN("open"),
+
+        CLOSED("closed");
+
+        private String value;
+
+        StatusEnum(String value) {
+            this.value = value;
+        }
+
+        @JsonCreator
+        public static StatusEnum fromValue(String text) {
+            for (StatusEnum b : StatusEnum.values()) {
+                if (String.valueOf(b.value).equals(text)) {
+                    return b;
+                }
+            }
+            return null;
+        }
+
+        @Override
+        @JsonValue
+        public String toString() {
+            return String.valueOf(value);
+        }
+    }
+}
